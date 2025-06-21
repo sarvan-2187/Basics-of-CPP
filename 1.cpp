2ad2fdcf01b6dd7d78c1fe318f5c1246cb649bcc
Nagarampalli Sarvan Kumar <sarvankumarnagarampalli478@gmail.com>
2025-06-21 16:13:12 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,20 +1,26 @@
+// C++ Tutorial
+
+//This is single line Comment
+
+/* This is a 
+Multi line Comment */
+
 //C++ is a Case Sensitive Language
-/*
-Commands:
+
+/* Commands:
 a) For Compilation: g++ fileName with Extension
 b) For Execution: ./a.exe --> (Windows)
-                  ./a.out --> (Mac & Linux)
-*/
+                  ./a.out --> (Mac & Linux) */
 
+#include <iostream> 
+/* This is preprocessor directive, Preprocessor directives in C++ are commands that 
+begin with the # symbol, and they’re processed before the actual compilation begins. */
 
-#include <iostream> //This is preprocessor directive
 using namespace std;
-/*
-cout is not unique i.e., if we have multiple files having multiple cout statements then C++ compiler gets confused, 
-in order to avoid that we use a namespace called std.
-*/
+/* cout is not unique i.e., if we have multiple files having multiple cout statements then C++ compiler gets confused, 
+in order to avoid that we use a namespace called std. */
 
 int main(){
-    cout << "Hello World! I'm Sarvan Kumar";
+    cout << "Hello World! I'm Sarvan Kumar"; // This is the output Statement.
     return 0; //As the function needs to return an integer value we use this statment.
 }
