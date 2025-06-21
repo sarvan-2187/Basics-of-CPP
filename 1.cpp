254ee040086e7a59630aa0a451e6751c525957c7
Nagarampalli Sarvan Kumar <sarvankumarnagarampalli478@gmail.com>
2025-06-21 15:55:05 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,1 +1,20 @@
+//C++ is a Case Sensitive Language
+/*
+Commands:
+a) For Compilation: g++ fileName with Extension
+b) For Execution: ./a.exe --> (Windows)
+                  ./a.out --> (Mac & Linux)
+*/
 
+
+#include <iostream> //This is preprocessor directive
+using namespace std;
+/*
+cout is not unique i.e., if we have multiple files having multiple cout statements then C++ compiler gets confused, 
+in order to avoid that we use a namespace called std.
+*/
+
+int main(){
+    cout << "Hello World! I'm Sarvan Kumar";
+    return 0; //As the function needs to return an integer value we use this statment.
+}
